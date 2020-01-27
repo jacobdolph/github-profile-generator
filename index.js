f7e63b3bed46f4b6dae6c4ece816ebb246b74a67
Jacob Dolph <jacob.dolph702@gmail.com>
2020-01-27 10:11:31 -0700

    added a url for the axios call

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 const util = require("util");
 
 const writeToFileAsync = util.promisify(fs.writeFile);
+// I need data, github username and a color
 const questions = [{
     type: "input",
     message: "What is your GITHUB username?",
@@ -14,14 +15,26 @@ const questions = [{
     message: "What is your favorite color?",
     name: "color"
 }]
+function getProfile(response) {
+    let userName = response.username
+    console.log(userName)
+    axios
+        .get(`https://api.github.com/users/${userName}/repos`)
+        .then(function (res) {
+            console.log(res.data.name);
+        });
+}
+//use inquirer to generate CLI questions
 inquirer
     .prompt(questions)
     .then(function (response) {
         console.log(response.username)
+        getProfile(response)
     })
     .catch(function (err) {
         console.log(err)
     });
+
 
 
 function writeToFile(fileName, data) {
