48558c256bd5234a115742db245b4bbbcd471a20
Jacob Dolph <jacob.dolph702@gmail.com>
2020-01-29 20:33:09 -0700

    got the async function to return to the command line the information that I need to create the html doc

--- index.js
+++ index.js
@@ -10,68 +10,98 @@ const appendToFileAsync = util.promisify(fs.appendFile);
 
 
 // I need data, github username and a color
-const questions = [{
-    type: "input",
-    message: "What is your GITHUB username?",
-    name: "username"
-},
-{
-    type: "input",
-    message: "What is your favorite color?",
-    name: "color",
-    choices: ["green", "pink", "red", "blue"]
 
-}];
-function writeToFile(fileName, data) {
+// function writeToFile(fileName, data) {
 
-}
+// }
 const gitUrl = `https://api.github.com/users/`;
 
+// inquirer
+//     .prompt(questions)
+//     .then(function (data) {
 
-inquirer
-    .prompt(questions)
-    .then(function (data) {
+//         let { userName } = data.username;
+//         let colorData = data.color;
 
-        let { userName } = data.username;
-        let colorData = data.color;
+//         axios
+//             .get(gitUrl + userName)
+//             .then(function (res) {
 
-        axios
-            .get(gitUrl + userName)
-            .then(function (res) {
+//                 let profileData = res.data;
 
-                let profileData = res.data;
-                let bioBio = profileData.bio.split(".");
-                let bioEmail = profileData.bio.split("\r\n\r\n");
-                let profileBio = bioBio[0];
-                let profileEmail = bioEmail[1];
-                const profile = {
-                    picture: profileData.avatar_url,
-                    blog: profileData.blog,
-                    location: profileData.location,
-                    github: profileData.html_url,
-                    email: profileEmail,
-                    bio: profileBio,
-                    followers: profileData.followers,
-                    following: profileData.following,
-                    repos: profileData.public_repos
-                }
-                console.log(colorData)
-                console.log(profile)
-                let newHtml = generateHTML(data)
-                console.log(newHtml)
-                await writeToFileAsync(`${userName}.html`, newHtml)
-            });
-    })
-    .catch(function (err) {
-        console.log(err)
-    });
+
+
+//                 console.log(colorData)
+//                 console.log(profile)
+//                 let newHtml = generateHTML(data)
+//                 console.log(newHtml)
+//                 await writeToFileAsync(`${userName}.html`, newHtml)
+//             });
+//     })
+//     .catch(function (err) {
+//         console.log(err)
+//     });
 
 
 
 
 
 async function init() {
+    try {
 
-
+        let { username } = await getName();
+        // const { profileName } = userName;
+        let { color } = await getColor();
+        let githubReturn = await gitHubCall(username);
+        let userStars = await getGithubStarred(username);
+        let userProfile = githubReturn.data
+        let bioBio = await userProfile.bio.split(".");
+        let profileBio = bioBio[0];
+        let profile = {
+            picture: userProfile.avatar_url,
+            blog: userProfile.blog,
+            location: userProfile.location,
+            github: userProfile.html_url,
+            bio: profileBio,
+            followers: userProfile.followers,
+            following: userProfile.following,
+            repos: userProfile.public_repos
+        }
+        console.log(color)
+        console.log(profile)
+        console.log(userStars.data.length)
+    } catch (err) {
+        console.log(err)
+    }
 }
-init();
+
+function getName() {
+    let username = inquirer
+        .prompt({
+            type: "input",
+            message: "What is your GITHUB username?",
+            name: "username"
+        })
+    return username
+}
+function getColor() {
+    let color = inquirer
+        .prompt({
+            type: "input",
+            message: "What is your favorite color?",
+            name: "color",
+            choices: ["green", "pink", "red", "blue"]
+        })
+    return color
+}
+function gitHubCall(username) {
+    let profile = axios
+        .get(`https://api.github.com/users/${username}`)
+    return profile
+}
+function getGithubStarred(username) {
+    const stars = axios
+        .get(`https://api.github.com/users/${username}/starred`)
+    return stars
+}
+init()
